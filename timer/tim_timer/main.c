b31d815f2e3fb338462306b44f566f6adba87a07
David Lin <linpeng1577@gmail.com>
2021-08-25 08:36:52 +0000

    [tim timer]修改示例

--- timer/tim_timer/main.c
+++ timer/tim_timer/main.c
@@ -11,12 +11,23 @@
 
 #include "lib_include.h"
 
-#define LED_PIN GPIO_PIN8
+#define LED_PIN   GPIO_PIN8
 
-#define LED_ON GPIO_SetPin(LED_PIN)
-#define LED_OFF GPIO_ClrPin(LED_PIN)
+#define LED_ON    GPIO_SetPin(LED_PIN)
+#define LED_OFF   GPIO_ClrPin(LED_PIN)
 
-void TIMER1_IrqHandler(void) {
+/**初始化ms定时器,中断周期50ms*/
+static void Timer_ms_Init(void)
+{
+    TIM_TimerInit(TIM1, TIM_TM_AUTO_LOAD, 50000);
+    PLIC_EnableIRQ(TIMER1_IRQn);
+    PLIC_SetPriority(TIMER1_IRQn, 1);
+    TIM_ClrIntFlag(TIM1);
+    TIM_EnableIRQ(TIM1);
+}
+
+void TIMER1_IrqHandler(void) 
+{
     static int tog = 0;
     if (tog) {
         LED_ON;
@@ -28,13 +39,18 @@ void TIMER1_IrqHandler(void) {
     TIM_ClrIntFlag(TIM1);
 }
 
-int main(void) {
+int main(void) 
+{
     GPIO_PinConfigure(LED_PIN, DISABLE, ENABLE, ENABLE, DISABLE, DISABLE);
-    TIM_TimerInit(TIM1, TIM_TM_AUTO_LOAD, 1000);
-    PLIC_EnableIRQ(TIMER1_IRQn);
-    PLIC_SetPriority(TIMER1_IRQn, 1);
-    TIM_ClrIntFlag(TIM1);
-    TIM_EnableIRQ(TIM1);
+    
+    SYSC->TIMCLKDIV = (SystemCoreClock / 1000000 - 1);
+    SYSC->CLKENCFG |= SYSC_CLKENCFG_TIM_PCK;
+
+    SYSC->CLKENCFG |= SYSC_CLKENCFG_TIM1_CNT;
+    TIMERS->CON |= BIT(0); // 使能TIMER1
+
+    Timer_ms_Init();
+    
     while (1)
         ;
 }
