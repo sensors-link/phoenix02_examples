ea48f54ab2d4a256aa77511e1ba87ad9a3d4519a
David Lin <linpeng1577@gmail.com>
2021-08-26 08:04:29 +0000

    [GPIO]更新用例
    使用定时器实现delay_us延时，替代for循环延时

--- gpio/gpio_out/main.c
+++ gpio/gpio_out/main.c
@@ -1,30 +1,55 @@
 /**
  * @file main.c
- * @author bifei.tang (you@domain.com)
+ * @author DavidLin
  * @brief
  * @version 0.1
- * @date 2020-05-28
+ * @date 2021-08-26
  *
- * @copyright Fanhai Data Tech. (c) 2020
+ * @copyright Fanhai Data Tech. (c) 2021
  *
  */
 
 #include "lib_include.h"
 
-#define TEST_PIN GPIO_PIN8
+#define LED_PIN  GPIO_PIN8
 
-#define PIN_ON GPIO_SetPin(TEST_PIN)
-#define PIN_OFF GPIO_ClrPin(TEST_PIN)
+#define LED_ON   GPIO_SetPin(LED_PIN)
+#define LED_OFF  GPIO_ClrPin(LED_PIN)
 
-int main(void) {
+#define TIME_DIFF(s,e) ((s)-(int)(e))
+
+int timer_init(void)
+{
+	// 初始化Timer1
+    SYSC->CLKENCFG |= SYSC_CLKENCFG_TIM_PCK | SYSC_CLKENCFG_TIM1_CNT;
+	SYSC->TIMCLKDIV = SystemCoreClock / 1000000 - 1; // 1M
+	TIMERS->INTCLR |= BIT(0); // 清TIMER1中断标记
+    // 32位自由运行模式
+	TIM1->CTCG1 = 0;
+	TIMERS->CON |= BIT(0);    // 使能TIMER1
+
+	return 0;
+}
+
+void delay_us(u32 delay)
+{
+    unsigned int start = TIM1->CTVAL;
+    while(TIME_DIFF(start, TIM1->CTVAL) < delay) __asm("NOP");
+}
+
+void gpio_out_example(void)
+{
     int i;
-    GPIO_PinConfigure(TEST_PIN, DISABLE, ENABLE, ENABLE, DISABLE, DISABLE);
-    while (1) {
-        PIN_OFF;
-        for (i = 10000; i > 0; i--)
-            ;
-        PIN_ON;
-        for (i = 10000; i > 0; i--)
-            ;
+
+    timer_init();
+
+    GPIO_PinConfigure(LED_PIN, DISABLE, ENABLE, ENABLE, DISABLE, DISABLE);
+
+    while (1)
+    {
+    	LED_OFF;
+    	delay_us(100000);
+        LED_ON;
+        delay_us(100000);
     }
 }
