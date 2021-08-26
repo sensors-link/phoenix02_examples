f1f3412b8497995fd84c02d0ba8cd34c84907e66
David Lin <linpeng1577@gmail.com>
2021-08-26 08:05:29 +0000

    [GPIO]调整代码缩进

--- gpio/gpio_out/main.c
+++ gpio/gpio_out/main.c
@@ -20,15 +20,15 @@
 
 int timer_init(void)
 {
-	// 初始化Timer1
+    // 初始化Timer1
     SYSC->CLKENCFG |= SYSC_CLKENCFG_TIM_PCK | SYSC_CLKENCFG_TIM1_CNT;
-	SYSC->TIMCLKDIV = SystemCoreClock / 1000000 - 1; // 1M
-	TIMERS->INTCLR |= BIT(0); // 清TIMER1中断标记
+    SYSC->TIMCLKDIV = SystemCoreClock / 1000000 - 1; // 1M
+    TIMERS->INTCLR |= BIT(0); // 清TIMER1中断标记
     // 32位自由运行模式
-	TIM1->CTCG1 = 0;
-	TIMERS->CON |= BIT(0);    // 使能TIMER1
-
-	return 0;
+    TIM1->CTCG1 = 0;
+    TIMERS->CON |= BIT(0);    // 使能TIMER1
+	
+    return 0;
 }
 
 void delay_us(u32 delay)
